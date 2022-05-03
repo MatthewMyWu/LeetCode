67bb74b184d8258634eb37555c07d5fdf8bc185f
MatthewMyWu <matthew6086888@hotmail.com>
2022-05-03 16:33:27 -0600

    Deleted babel

--- dist/581 Shortest Subarray/tester.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const runner_1 = require("./runner");
-(0, runner_1.findUnsortedSubarray)([1, 3, 2, 2, 2]);
-console.log("Working");
-//# sourceMappingURL=tester.js.map
--- dist/Tester.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-console.log("Hello World");
-const code = { word: "target" };
-const firstObject = { target: code };
-const secondObject = Object.assign({}, firstObject);
-secondObject.target.word = "Infiltrated";
-console.log("First object: " + firstObject.target.word);
-console.log("Second object: " + secondObject.target.word);
-//# sourceMappingURL=Tester.js.map
